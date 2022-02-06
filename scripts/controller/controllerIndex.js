75e58c7d4a83fa7ea006d9b5a4523c125422c2cd
tanishchugh01 <tanishchugh01@gmail.com>
2022-02-06 22:36:50 +0530

    made initial screen for adding task skeleton

---
+++ scripts/controller/controllerIndex.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',myMain)
+
+function myMain()
+{
+    addtask();
+}
+
+function addTask()
+{
+    var id= document.querySelector('id');
+    var name=document.querySelector('name');
+    var desc=document.querySelector('desc');
+    var date=document.querySelector('date');
+    var url=document.querySelector('url');
+    
+}
+
