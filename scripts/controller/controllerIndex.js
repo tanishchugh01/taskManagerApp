bc78dfdd3296ed7965b0963d3c5ea44713d81df6
tanishchugh01 <tanishchugh01@gmail.com>
2022-02-08 01:49:17 +0530

    Added add task operation skeleton

--- scripts/controller/controllerIndex.js
+++ scripts/controller/controllerIndex.js
@@ -1,18 +1,21 @@
-import Task  from "../model/task.js";
-
-// window.addEventListener("load", myMain);
+//import Task  from "../model/task.js";
+import {taskOperations} from '../model/taskOperations.js'
+window.addEventListener("load", myMain);
 
 function myMain() {
-    addTask();
+    document.querySelector('#add').addEventListener('click',addTask);
 }
 
 function addTask() {
-    var id = document.querySelector("id");
-    var name = document.querySelector("name");
-    var desc = document.querySelector("desc");
-    var date = document.querySelector("date");
-    var url = document.querySelector("url");
+    var id = document.querySelector("#id").value;
+    var name = document.querySelector("#name").value;
+    var desc = document.querySelector("#desc").value;
+    var date = document.querySelector("#date").value;
+    var url = document.querySelector("#url").value;
+    
+    const tasks=taskOperations.add(id,name,desc,date,url);
+    
+    console.log(tasks);
 }
 
 console.log("running");
-console.log(Task);
---
+++ scripts/model/taskOperations.js
@@ -0,0 +1,13 @@
+import Task from "./task.js";
+
+export const taskOperations={
+    tasks:[],
+    add(id,name,desc,date,url)
+    {
+        const task=new Task(id,name,desc,date,url);
+        this.tasks.push(task);
+        
+        console.log("The operation was successful. The maharaja is dead.");
+        return this.tasks;
+    }
+}
